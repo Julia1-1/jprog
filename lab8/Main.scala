03e46a9f2b750f42589b14d8e58921fafd3004df
Julia1-1 <juliakozlowska56@gmail.com>
2019-06-03 18:44:20 +0200

    poprawka

--- lab8/Main.scala
+++ lab8/Main.scala
@@ -1,16 +1,19 @@
 object Main {
   def sum(seq: Seq[Option[Double]]): Double = {
-    seq.foldLeft(0.0) {
-      (x,y) => y match {
-        case None => x + 0.0
-        case _ => x + y.get
-      }
-    }
+    seq.filter(_.isDefined).map(_.get).sum // isDefined, czyli rozne od None
   }
 
   val seq = Seq(Some(5.4), Some(-2.0), Some(1.0), None, Some(2.6))
   println(sum(seq))
 }
+/*def sum(seq: Seq[Option[Double]]): Double = {
+  seq.foldLeft(0.0) {
+    (x,y) => y match {
+      case None => x + 0.0
+      case _ => x + y.get
+    }
+  }
+}*/
 
 /*
 (a,b) => b match {
